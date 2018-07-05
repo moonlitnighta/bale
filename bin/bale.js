607b25dc5e1cc6d8123bd753ca99025ad43e00ce
fengfugen <moonlitnighta@gmail.com>
2018-07-05 18:15:20 +0800

    开发 vue plugin 完毕

--- bin/bale.js
+++ bin/bale.js
@@ -15,6 +15,8 @@ let config = null;
 //获取bale执行的所在目录 的 绝对路径
 let balePath = options.balePath = process.cwd();
 
+let objectTostring = Object.prototype.toString;
+
 //---------------------------------------------处理配置文件的参数---------------------------------------------//
 if(args.config){
 
@@ -22,21 +24,18 @@ if(args.config){
 	config = require(mergePath(balePath , args.config));
 
 	//如果配置文件内容为空 || 配置文件输出不为对象 则终止
-	if(!config || Object.prototype.toString.call(config) != '[object Object]'){
+	if(!config || objectTostring.call(config) != '[object Object]'){
 		global._bale_.error({
 			message:'Error: 配置文件错误' + args.config,
 			exit:true
 		})
 	}
 
-	//配置文件所在的目录 的 绝对路径
-	config.configPath = mergePath(balePath , args.config);
-
 	//根据配置的入口参数 获取 入口文件的绝对路径
-	config.entryPath = mergePath(path.dirname(config.configPath) , config.entry);
+	config.entryPath = mergePath(balePath , config.entry);
 
 	//根据配置的出口参数 获取 出口文件的绝对路径
-	config.output = config.output && outputConfig(config.output , path.dirname(config.configPath));
+	config.output = config.output && outputConfig(config.output ,balePath);
 	//........
 
 	//和命令行参数合并
@@ -75,9 +74,10 @@ options.loaders = args.loaders || [];
 //内置loaders
 options.internalLoaders = args.internalLoaders || {};
 
+//babel
 //如果配置babel 则.ast选项强制为false presets 默认为es2015;
 if(options.internalLoaders.babel){
-	if(Object.prototype.toString.call(options.internalLoaders.babel) != '[object Object]'){
+	if(objectTostring.call(options.internalLoaders.babel) != '[object Object]'){
 		options.internalLoaders.babel = {};
 	}
 	options.internalLoaders.babel.options = options.internalLoaders.babel.options || {};
@@ -85,6 +85,7 @@ if(options.internalLoaders.babel){
 	//强制选项
 	options.internalLoaders.babel.options.ast = false;
 	//默认选项
+	options.internalLoaders.babel.exclude = options.internalLoaders.babel.exclude || /(bower_components|node_modules)/;
 	options.internalLoaders.babel.options.presets = options.internalLoaders.babel.options.presets || 'es2015';
 	options.internalLoaders.babel.options.babelrc = !!options.internalLoaders.babel.options.babelrc;
 	options.internalLoaders.babel.options.comments = !!options.internalLoaders.babel.options.comments;
@@ -93,10 +94,12 @@ if(options.internalLoaders.babel){
 
 //cssnano 压缩css 选项
 options.internalLoaders.cssnano = options.internalLoaders.cssnano || {};
+options.internalLoaders.cssnano.discardComments = options.internalLoaders.cssnano.discardComments || 
+												  {removeAll: true};
 
 //js压缩选项
 if(options.internalLoaders.uglifyJs){
-	if(Object.prototype.toString.call(options.internalLoaders.uglifyJs) != '[object Object]'){
+	if(objectTostring.call(options.internalLoaders.uglifyJs) != '[object Object]'){
 		options.internalLoaders.uglifyJs = {};
 	}
 }
@@ -122,6 +125,72 @@ options.internalLoaders.image = options.internalLoaders.image || {};
 options.internalLoaders.image.limit = options.internalLoaders.image.limit && 
 								      options.internalLoaders.image.limit < 50 ? 
 								      options.internalLoaders.image.limit : 25;
+//内置功能配置
+options.internalPlugin = args.internalPlugin || {};		
+
+//合并css				      
+if(options.internalPlugin.mergeStyle){
+	let config = options.internalPlugin.mergeStyle;
+	if(objectTostring.call(config) == '[object Object]'){
+
+		let t = objectTostring.call(config.extensions);
+		if(t != '[object Array]' && t != '[object String]'){
+			config.extensions = ['.css'];
+		}
+		if(config.name){
+			if(objectTostring.call(config.name) != '[object String]'){
+				global._bale_.error({
+					message:'Error: 参数错误: mergeStyle.name 必须为string',
+					exit:true
+				})
+			}
+		} else {
+			config.name = path.parse(options.output.fileName).name + '.css';
+		}
+	} else {
+		global._bale_.error({
+			message:'Error: 参数错误: mergeStyle 必须为object',
+			exit:true
+		})
+	}
+}
+//生成html
+if(options.internalPlugin.htmlTemplate){
+	let config = options.internalPlugin.htmlTemplate;
+	if(objectTostring.call(config) == '[object Object]'){
+		config.title = config.title || 'bale';
+		config.chunks = config.chunks || 'all';
+		config.filename = config.filename || 'index.html';
+		config.inject = config.inject || 'body';
+	} else {
+		global._bale_.error({
+			message:'Error: 参数错误: htmlTemplate 必须为object',
+			exit:true
+		})
+	}
+}
+
+//构建事件
+options.events = args.events || {};
+if(objectTostring.call(options.events) != '[object Object]'){
+	global._bale_.error({
+		message:'Error: 参数错误: options.events 必须为object',
+		exit:true
+	})
+} else {
+	for(let k in options.events){
+		let type = objectTostring.call(options.events[k]);
+		if(type == '[object Function]' || type == '[object Array]'){
+			let arr = type == '[object Function]' ? [options.events[k]] : options.events[k];
+			options.events[k] = arr;
+		} else {
+			global._bale_.error({
+				message:'Error: 参数错误: '+k+' 必须为object',
+				exit:true
+			})
+		}
+	}
+}
 
 //------------------------------------------根据命令行参数及配置文件生成编译对象-----------------------------------------//
 
@@ -154,7 +223,7 @@ function mergePath(publicPath , _path){
 function outputConfig(opt , publicPath){
 	let config = {};
 
-	if(Object.prototype.toString.call(opt) == '[object String]'){
+	if(objectTostring.call(opt) == '[object String]'){
 		try {
 			config.path = mergePath(publicPath , opt);
 			config.fileName = path.basename(config.path);
@@ -163,16 +232,32 @@ function outputConfig(opt , publicPath){
 			config = undefined;
 		}
 			
-	} else if (Object.prototype.toString.call(opt) == '[object Object]'){
+	} else if (objectTostring.call(opt) == '[object Object]'){
 		try {
 			config.path = mergePath(publicPath , opt.path);
 			config.fileName = opt.fileName || 'output.js';
 			config.context = path.dirname(path.join(config.path , config.fileName));
+			config.vendor = opt.vendor;
 		} catch (err) {
 			config = undefined;
 		}
 	} else {
 		config = undefined;
+	}
+
+	if(config.vendor && !config.vendor.name){
+		config.vendor.name = 'vendor.js';
+	}
+
+	if(config.vendor && !config.vendor.vendors){
+		config.vendor.vendors = [];
+	}
+
+	if(config.vendor && config.vendors && objectTostring.call(config.vendors) != '[object Array]'){
+		global._bale_.error({
+			message:'Error: 参数错误: vendor.vendors 不是有效数组',
+			exit:true
+		})
 	}
 
 	return config;
